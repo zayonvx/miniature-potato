d91d14c76086990559ea3175d9e65f7841f969b2
vladislav zaionchkovskiy <zayonvx@gmail.com>
2022-02-14 00:27:00 +0300

    fix Core project, swagger add

--- OnePlan.Core/Configuration/StartupDependencies.cs
+++ OnePlan.Core/Configuration/StartupDependencies.cs
@@ -1,19 +1,20 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using OnePlan.Data.Context;
 
-namespace OnePlan.Core.Dependencies;
+namespace OnePlan.Data.Dependencies;
 
 public static class StartupDependencies
 {
     public static void RegisterDependencies(this IServiceCollection services, IConfiguration configuration)
     {
-        services.RegisterDependencies(configuration);
+        services.RegisterDatabase(configuration);
     }
 
-    public static void RegisterDatabase(this IServiceCollection services, IConfiguration configuration)
+    private static void RegisterDatabase(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddDbContext<AppDbContext>(options =>
-            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"))
-        );
+            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
     }
 }
--- OnePlan.Web/Program.cs
+++ OnePlan.Web/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.OpenApi.Models;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +8,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo {Title = "OnePlan,Web", Version = "v1"});
+});
 
 var app = builder.Build();
 
@@ -13,7 +19,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "OnePlan.Web v1"));
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "OnenPlan.Web v1"));
 }
 
 app.UseHttpsRedirection();
